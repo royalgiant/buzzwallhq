00730ad576646d6af7d519da52457683fef1079d
Donald Lee <donaldlee50@gmail.com>
2024-09-09 16:25:56 -0500

    Add Privacy and Terms

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :home, only: [:index]
+
+  scope controller: :static do
+    get :terms
+    get :privacy
+  end
 end
