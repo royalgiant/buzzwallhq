6ce0360ff60d86698192a9330a428e52f1c4a049
Donald Lee <donaldlee50@gmail.com>
2024-10-18 21:15:40 -0500

    M

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,12 @@
 # config/initializers/redis.rb
-if Rails.env.production? && ENV["REDIS_URL"].present?
-  uri = URI.parse(ENV["REDIS_URL"])
-  Redis.current = Redis.new(url: ENV["REDIS_URL"])
+Rails.logger.info("Rails.env.production?: #{Rails.env.production?}")
+redis_url = Rails.application.credentials.dig(Rails.env.to_sym, :redis_url) || ENV['REDIS_URL']
+Rails.logger.info("ENV['REDIS_URL']: #{redis_url}")
+if Rails.env.production? && redis_url.present?
+  Rails.logger.info("Redis is in production")
+  uri = URI.parse(redis_url)
+  Redis.current = Redis.new(url: redis_url)
 else
+  Rails.logger.info("Redis is in development")
   Redis.current = Redis.new(host: 'localhost', port: 6379, db: 0)
 end
